9892e5b99447bac792b88f30f0627af23ead6eb3
Tiago Jacinto <tiagojacintodev@gmail.com>
2023-01-18 11:08:55 +0000

    List Key Of Task Model Is Required

--- src/models/Task.js
+++ src/models/Task.js
@@ -2,7 +2,7 @@ const mongoose = require("mongoose");
 
 const taskSchema = mongoose.Schema({
   name: { type: String, required: true },
-  list: { type: mongoose.Schema.Types.ObjectId, ref: "List" },
+  list: { type: mongoose.Schema.Types.ObjectId, ref: "List", required: true },
   description: String,
   dueDate: Date,
   priority: { type: String, enum: ["Low", "Medium", "High", "Urgent"] },
