339435779c809879cbe5e2df3867d4cf47222618
Tiago Jacinto <tiagojacintodev@gmail.com>
2023-01-18 10:05:27 +0000

    Accept URL Encoded Body

--- src/app.js
+++ src/app.js
@@ -3,5 +3,12 @@ const app = express();
 require("dotenv").config();
 require("./config/db")();
 
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+
+app.use("/api/lists", require("./routes/listRoutes"));
+
+app.use(require("./middleware/errorHandler"));
+
 const port = process.env.PORT || 8000;
-app.listen(port);
+app.listen(port, () => console.log(`Server started on port ${port}`));
