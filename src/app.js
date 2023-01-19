fb627d1fb91441dd3a1a69b7abf10eb02b19d28d
Tiago Jacinto <tiagojacintodev@gmail.com>
2023-01-19 14:21:59 +0000

    Add Authentication Middleware

--- src/app.js
+++ src/app.js
@@ -2,10 +2,15 @@ const express = require("express");
 const app = express();
 require("dotenv").config();
 require("./config/connectDB")();
+require("./auth/passport");
+const passport = require("passport");
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
+app.use("/api/v1", require("./api/index"));
+
+app.use(passport.authenticate("jwt", { session: false }));
 app.use("/api/lists", require("./routes/listRoutes"));
 app.use("/api/tasks", require("./routes/taskRoutes"));
 
