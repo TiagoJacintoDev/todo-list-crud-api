2c6fea44908a90b21d3d9866d5ae8de383a088e1
Tiago Jacinto <tiagojacintodev@gmail.com>
2023-01-20 10:16:06 +0000

    Remove Passport Authentication

--- src/app.js
+++ src/app.js
@@ -2,29 +2,12 @@ const express = require("express");
 const app = express();
 require("dotenv").config();
 require("./config/connectDB")();
-require("./auth/passport");
-require("./auth/passportGoogleSSO");
-const passport = require("passport");
-const cookieSession = require("cookie-session");
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
-app.use("/api/v1", require("./api/index"));
-
-app.use(passport.authenticate("jwt", { session: false }));
-app.use("/api/lists", require("./routes/listRoutes"));
-app.use("/api/tasks", require("./routes/taskRoutes"));
-
-app.use(
-  cookieSession({
-    maxAge: 24 * 60 * 60 * 1000,
-    keys: [process.env.COOKIE_KEY],
-  })
-);
-
-app.use(passport.initialize());
-app.use(passport.session());
+app.use("/api/lists", authHandler, require("./routes/listRoutes"));
+app.use("/api/tasks", authHandler, require("./routes/taskRoutes"));
 
 app.use(require("./middleware/errorHandler"));
 
