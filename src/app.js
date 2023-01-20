0ed59d3a14d2b2c9b00f1d376f062bfee4ca4f1b
Tiago Jacinto <tiagojacintodev@gmail.com>
2023-01-20 09:43:36 +0000

    Add Cookie Session Support

--- src/app.js
+++ src/app.js
@@ -3,7 +3,9 @@ const app = express();
 require("dotenv").config();
 require("./config/connectDB")();
 require("./auth/passport");
+require("./auth/passportGoogleSSO");
 const passport = require("passport");
+const cookieSession = require("cookie-session");
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
@@ -13,6 +15,16 @@ app.use("/api/v1", require("./api/index"));
 app.use(passport.authenticate("jwt", { session: false }));
 app.use("/api/lists", require("./routes/listRoutes"));
 app.use("/api/tasks", require("./routes/taskRoutes"));
+
+app.use(
+  cookieSession({
+    maxAge: 24 * 60 * 60 * 1000,
+    keys: [process.env.COOKIE_KEY],
+  })
+);
+
+app.use(passport.initialize());
+app.use(passport.session());
 
 app.use(require("./middleware/errorHandler"));
 
