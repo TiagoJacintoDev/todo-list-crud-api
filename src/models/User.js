b5974a9de9766b041b72ecdeec1e78d84f67e2da
Tiago Jacinto <tiagojacintodev@gmail.com>
2023-01-20 09:44:02 +0000

    User Schema Fields are optional & Implement Static

--- src/models/User.js
+++ src/models/User.js
@@ -1,18 +1,22 @@
 const mongoose = require("mongoose");
 
-const userSchema = mongoose.Schema({
-  fullName: {
-    type: String,
-    required: true,
+const userSchema = mongoose.Schema(
+  {
+    fullName: String,
+    email: String,
+    password: String,
+    googleId: String,
+    picture: String,
   },
-  email: {
-    type: String,
-    required: true,
-  },
-  password: {
-    type: String,
-    required: true,
-  },
-});
+  {
+    statics: {
+      async findOneOrCreate(condition, doc) {
+        const one = await this.findOne(condition);
+
+        return one || this.create(doc);
+      },
+    },
+  }
+);
 
 module.exports = mongoose.model("User", userSchema);
